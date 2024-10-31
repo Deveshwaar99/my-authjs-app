f7f64673167476b01a4411a42d95f441601d1d4b
Deveshwaar <deveshwaar99@gmail.com>
2024-10-31 23:12:05 +0530

    feat: wrap social image links with styled Button components for improved UI

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import { Button } from '../ui/button'
 
 const githubLogo = '/github.svg'
 const googleLogo = '/google.svg'
@@ -8,13 +9,16 @@ const googleLogo = '/google.svg'
 function Social() {
   return (
     <div className="flex w-full items-center justify-center gap-x-4">
-      <a href="a" className="relative size-5">
-        <Image fill src={googleLogo} alt="google_logo" />
-      </a>
-
-      <a href="s" className="relative size-5">
-        <Image fill src={githubLogo} alt="github_logo" className="object-cover" />
-      </a>
+      <Button variant="outline" className="inline-flex w-1/2 items-center justify-center">
+        <a href="a" className="relative size-5">
+          <Image fill src={googleLogo} alt="google_logo" />
+        </a>
+      </Button>
+      <Button variant="outline" className="inline-flex w-1/2 items-center justify-center">
+        <a href="s" className="relative size-5">
+          <Image fill src={githubLogo} alt="github_logo" className="object-cover" />
+        </a>
+      </Button>
     </div>
   )
 }
