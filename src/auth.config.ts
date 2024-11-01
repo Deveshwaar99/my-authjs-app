4264d98d6ba708c4ab29ea80d76c4b3314a6b8b6
Deveshwaar <deveshwaar99@gmail.com>
2024-11-01 22:59:20 +0530

    feat(auth): add GitHub and Google authentication providers
    - Integrated GitHub and Google as additional authentication providers
    - Used `process.env` for client ID and secret configuration
    - Retained existing credentials-based authentication logic

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,20 @@ import type { NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import { getUserByEmail } from './data/user'
 import { LoginSchema } from './schema'
-
+import Github from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 export default {
-  providers: [Credentials({ authorize })],
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({ authorize }),
+  ],
 } satisfies NextAuthConfig
 
 async function authorize(credentials: unknown) {
