37350ee0d99eeb22b6fb40a24e1cf3facecc84a9
Deveshwaar <deveshwaar99@gmail.com>
2024-11-01 23:03:24 +0530

    feat(auth): add provider login with transition handling to Social component
    - Integrated `useTransition` for sign-in feedback
    - Added `onClick` to trigger provider login dynamically
    - Disabled buttons during transitions for better UX

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,23 +1,44 @@
 'use client'
 
+import { DEFAULT_LOGIN_REDIRECT } from '@/middleware'
+import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import { useTransition } from 'react'
 import { Button } from '../ui/button'
+import { loginWithProvider } from '@/actions/login-action'
 
 const githubLogo = '/github.svg'
 const googleLogo = '/google.svg'
 
 function Social() {
+  const [isPending, startTransition] = useTransition()
+  const onClick = (provider: 'google' | 'github') => {
+    startTransition(async () => {
+      // signIn(provider, { redirectTo: DEFAULT_LOGIN_REDIRECT })
+      await loginWithProvider(provider)
+    })
+  }
   return (
     <div className="flex w-full items-center justify-center gap-x-4">
-      <Button variant="outline" className="inline-flex w-1/2 items-center justify-center">
-        <a href="a" className="relative size-5">
+      <Button
+        onClick={() => onClick('google')}
+        disabled={isPending}
+        variant="outline"
+        className="inline-flex w-1/2 items-center justify-center"
+      >
+        <span className="relative size-5">
           <Image fill src={googleLogo} alt="google_logo" />
-        </a>
+        </span>
       </Button>
-      <Button variant="outline" className="inline-flex w-1/2 items-center justify-center">
-        <a href="s" className="relative size-5">
+      <Button
+        onClick={() => onClick('github')}
+        disabled={isPending}
+        variant="outline"
+        className="inline-flex w-1/2 items-center justify-center"
+      >
+        <span className="relative size-5">
           <Image fill src={githubLogo} alt="github_logo" className="object-cover" />
-        </a>
+        </span>
       </Button>
     </div>
   )
