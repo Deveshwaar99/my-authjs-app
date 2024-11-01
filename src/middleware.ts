ab811be22961c61ffcee8930b68d73d5ab4a3e01
Deveshwaar <deveshwaar99@gmail.com>
2024-11-01 23:00:54 +0530

    refactor(auth): improve authentication middleware with NextResponse
    - Replaced `Response.redirect` with `NextResponse.redirect` for better server integration
    - Improved readability and structure of authentication checks
    - Ensured non-API routes without authentication redirect to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,33 @@
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
+import { NextResponse } from 'next/server'
 
 const { auth } = NextAuth(authConfig)
 
-// List of routes that do not require authentication
-const PUBLIC_ROUTES = ['/', '/auth/login', '/auth/register']
-
-// Prefix for API routes that require authentication
-const API_AUTH_PREFIX = '/api/auth'
-
-// Default redirect URL for authenticated users
-export const DEFAULT_LOGIN_REDIRECT = '/settings'
+const PUBLIC_ROUTES = ['/', '/auth/login', '/auth/register'] // List of routes that do not require authentication
+const API_AUTH_PREFIX = '/api/auth' // Prefix for API routes that require authentication
+export const DEFAULT_LOGIN_REDIRECT = '/settings' // Default redirect URL for authenticated users
 
 export default auth(req => {
-  const { nextUrl, auth: userAuth } = req
-  const isLoggedIn = !!userAuth
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(API_AUTH_PREFIX)
   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
-  // console.log({ isApiAuthRoute, isAuthRoute, isPublicRoute, isLoggedIn })
-  if (isApiAuthRoute) return
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
 
   if (isLoggedIn && (nextUrl.pathname === '/auth/login' || nextUrl.pathname === '/auth/register')) {
-    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL('/auth/login', nextUrl))
+    return NextResponse.redirect(new URL('/auth/login', nextUrl))
   }
 
-  return
+  return NextResponse.next()
 })
 
 export const config = {
