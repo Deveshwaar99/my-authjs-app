537366aa2c09ac7e7ad725e60c171b68bf83f25b
Deveshwaar <deveshwaar99@gmail.com>
2024-11-01 18:25:44 +0530

    refactor(db): migrate from vercel-postgres to postgres-js
    Replace `drizzle-orm/vercel-postgres` with `drizzle-orm/postgres-js` and initialize the `postgres` client. Add `biome-ignore` to handle non-null assertion.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,14 @@
 import 'server-only'
 
-import { drizzle } from 'drizzle-orm/vercel-postgres'
-import { sql } from '@vercel/postgres'
+// import { drizzle } from 'drizzle-orm/vercel-postgres'
+// import { sql } from '@vercel/postgres'
 
-export const db = drizzle()
+// export const db = drizzle()
+
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+const connectionString = process.env.POSTGRES_URL!
+const client = postgres(connectionString)
+export const db = drizzle(client)
