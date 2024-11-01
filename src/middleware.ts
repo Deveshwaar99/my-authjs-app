bd7d10d527b9bdf3668d529f2287919f6f16e192
Deveshwaar <deveshwaar99@gmail.com>
2024-11-01 18:27:33 +0530

    refactor(auth): streamline route handling and authentication logic
    Consolidate PUBLIC_ROUTES by including authentication routes. Simplify authentication checks and update matcher configuration for improved clarity.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,7 @@ import authConfig from './auth.config'
 const { auth } = NextAuth(authConfig)
 
 // List of routes that do not require authentication
-const PUBLIC_ROUTES = ['/']
-
-// List of routes related to authentication, such as login and registration
-const AUTH_ROUTES = ['/auth/login', '/auth/register']
+const PUBLIC_ROUTES = ['/', '/auth/login', '/auth/register']
 
 // Prefix for API routes that require authentication
 const API_AUTH_PREFIX = '/api/auth'
@@ -20,16 +17,12 @@ export default auth(req => {
   const isLoggedIn = !!userAuth
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(API_AUTH_PREFIX)
-  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname)
   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
-  console.log({ isApiAuthRoute, isAuthRoute, isPublicRoute, isLoggedIn })
+  // console.log({ isApiAuthRoute, isAuthRoute, isPublicRoute, isLoggedIn })
   if (isApiAuthRoute) return
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    }
-    return
+  if (isLoggedIn && (nextUrl.pathname === '/auth/login' || nextUrl.pathname === '/auth/register')) {
+    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
   }
 
   if (!isLoggedIn && !isPublicRoute) {
@@ -40,10 +33,5 @@ export default auth(req => {
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
