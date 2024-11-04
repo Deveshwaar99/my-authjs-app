d1669119ab3231fcf4c9d7b0d3474d2909b98004
Deveshwaar <deveshwaar99@gmail.com>
2024-11-04 21:57:41 +0530

    fix(resend-email):Correct Confirmation link

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,7 +2,8 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendVerificationEmail(email: string, token: string) {
-  const CONFIRMATION_LINK = `http://localhost:3000/auth/new-verification=${token}`
+  const CONFIRMATION_LINK = `http://localhost:3000/auth/new-verification?token=${token}`
+
   const { error } = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
     to: [email],
