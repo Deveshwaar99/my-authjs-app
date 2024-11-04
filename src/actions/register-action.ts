833060adc87317694a5af76e048e77f86443ef11
Deveshwaar <deveshwaar99@gmail.com>
2024-11-04 12:34:03 +0530

    feat(register): implement transaction for user registration with verification token management
    - Added database transaction handling in `registerAction`.
    - Integrated deletion of existing verification tokens before creating a new one.
    - Implemented generation of a new verification token after successful registration.
    - Enhanced error handling to ensure smooth user registration and verification flow.

--- src/actions/register-action.ts
+++ src/actions/register-action.ts
@@ -1,28 +1,55 @@
 'use server'
 
 import { getUserByEmail } from '@/data/user'
+import {
+  type CustomPgTransaction,
+  deleteExistingVerificationTokens,
+  generateVerificationToken,
+} from '@/data/verificationToken'
 import { db } from '@/db'
 import { users } from '@/db/schema'
 import { RegisterSchemaBackend } from '@/schema'
 import bcryptjs from 'bcryptjs'
 
-export async function registerAction(values: unknown): Promise<{
-  status: 'error' | 'success'
-  message: string
-}> {
+export async function registerAction(values: unknown) {
   const validatedFields = RegisterSchemaBackend.safeParse(values)
   if (!validatedFields.success) {
-    return { status: 'error', message: 'Invalid input fields!' }
+    return {
+      status: 'error' as const,
+      message: 'Invalid input fields!',
+    }
   }
+
   const { name, email, password } = validatedFields.data
+
   const existingUser = await getUserByEmail(email)
   if (existingUser) {
-    return { status: 'error', message: 'User already exists' }
+    return {
+      status: 'error' as const,
+      message: 'User already exists',
+    }
   }
 
   const hashedPassword = await bcryptjs.hash(password, 10)
 
-  await db.insert(users).values({ name, email, password: hashedPassword })
+  try {
+    await db.transaction(async trx => {
+      await trx.insert(users).values({ name, email, password: hashedPassword })
+      await deleteExistingVerificationTokens(email, trx as CustomPgTransaction)
+      await generateVerificationToken(email, trx as CustomPgTransaction)
+    })
 
-  return { status: 'success', message: 'Account created successfully!' }
+    return {
+      status: 'success' as const,
+      message: 'Confirmation email sent!',
+    }
+  } catch (error) {
+    console.error('[REGISTRATION_ERROR]:', error)
+    return {
+      status: 'error' as const,
+      message: 'Registration failed. Please try again later.',
+    }
+  }
 }
+
+export type RegistrationResult = Awaited<ReturnType<typeof registerAction>>
