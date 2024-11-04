56e195c82b0f5672410a0ec2a3dac7198fe792c7
Deveshwaar <deveshwaar99@gmail.com>
2024-11-04 12:32:50 +0530

    feat(verification-token): add functions to manage email verification tokens in database

---
+++ src/data/verificationToken.ts
@@ -0,0 +1,74 @@
+'use server'
+
+import { db } from '@/db'
+import type * as schema from '@/db/schema'
+import { verificationTokens } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+import type { PgTransaction } from 'drizzle-orm/pg-core'
+import type { PostgresJsDatabase, PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js'
+import { nanoid } from 'nanoid'
+import type { Sql } from 'postgres'
+
+export type CustomPostgresDatabase = PostgresJsDatabase<typeof import('@/db/schema')> & {
+  $client: Sql<Record<string, unknown>>
+}
+
+export type CustomPgTransaction = PgTransaction<PostgresJsQueryResultHKT, typeof schema>
+
+export async function generateVerificationToken(
+  email: string,
+  trx: CustomPgTransaction | CustomPostgresDatabase = db,
+) {
+  try {
+    const token = nanoid()
+    const expires = new Date(Date.now() + 60 * 60 * 1000) // 1 hour from now
+
+    const [verificationToken] = await trx
+      .insert(verificationTokens)
+      .values({ token, email, expires })
+      .returning()
+
+    return verificationToken
+  } catch (error) {
+    console.error(`[GENERATE_VERIFICATION_TOKEN_ERROR] Email: ${email}`, error)
+    throw error
+  }
+}
+
+export async function deleteExistingVerificationTokens(
+  email: string,
+  trx: CustomPgTransaction | CustomPostgresDatabase = db,
+) {
+  try {
+    await trx.delete(verificationTokens).where(eq(verificationTokens.email, email))
+  } catch (error) {
+    console.error(`[DELETE_VERIFICATION_TOKENS_ERROR] Email: ${email}`, error)
+    throw error
+  }
+}
+
+export async function getVerificationTokenByEmail(email: string) {
+  try {
+    return await db
+      .select()
+      .from(verificationTokens)
+      .where(eq(verificationTokens.email, email))
+      .then(res => res[0])
+  } catch (error) {
+    console.error('[GET_VERIFICATION_TOKEN_BY_EMAIL_ERROR]', error)
+    return null
+  }
+}
+
+export async function getVerificationTokenByToken(token: string) {
+  try {
+    return await db
+      .select()
+      .from(verificationTokens)
+      .where(eq(verificationTokens.token, token))
+      .then(res => res[0])
+  } catch (error) {
+    console.error('[GET_VERIFICATION_TOKEN_BY_TOKEN_ERROR]', error)
+    return null
+  }
+}
