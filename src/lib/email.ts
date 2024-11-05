6652e3fb6ef61e190c140a95bd0478e049d4d449
Deveshwaar <deveshwaar99@gmail.com>
2024-11-05 16:44:37 +0530

    feat: add function to send password reset email with generated token and reset link

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -15,3 +15,17 @@ export async function sendVerificationEmail(email: string, token: string) {
     console.error('[SEND_VERIFICATION_EMAIL_ERROR]', error)
   }
 }
+export async function sendPasswordResetEmail({ email, token }: { email: string; token: string }) {
+  const RESET_LINK = `http://localhost:3000/auth/new-password?token=${token}`
+
+  const { error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: [email],
+    subject: 'Reset your password',
+    html: `<p>Click <a href='${RESET_LINK}'>here </a> to reset password</p>`,
+  })
+
+  if (error) {
+    console.error('[SEND_VERIFICATION_EMAIL_ERROR]', error)
+  }
+}
