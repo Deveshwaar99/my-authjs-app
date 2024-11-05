210f7c1ad5865da51e9a84fe81faca601030c1ba
Deveshwaar <deveshwaar99@gmail.com>
2024-11-05 18:30:07 +0530

    feat(password-reset): implement server action for updating password with token validation
    -Added setNewPasswordAction to handle secure   password updates
    -Included token verification and expiration checks
    -Enhanced user validation and error handling for comprehensive responses
    -Implemented password update logic and token  deletion for improved security
    -Logged errors for better issue tracking and   debugging

--- src/actions/reset-password-action.ts
+++ src/actions/reset-password-action.ts
@@ -2,12 +2,14 @@
 
 import {
   deletePasswordResetTokenByEmail,
+  deletePasswordResetTokenById,
   generatePasswordResetToken,
+  getPasswoordResetTokenByToken,
 } from '@/data/password-reset-token'
-import { getUserByEmail } from '@/data/user'
+import { getUserByEmail, updateUserPassword } from '@/data/user'
 import { sendPasswordResetEmail } from '@/lib/email'
-import { PasswordResetSchema } from '@/schema'
-import { z } from 'zod'
+import { NewPasswordSchema, PasswordResetSchema } from '@/schema'
+import type { z } from 'zod'
 
 export async function resetPasswordAction(values: unknown) {
   const validatedFields = PasswordResetSchema.safeParse(values)
@@ -29,6 +31,64 @@ export async function resetPasswordAction(values: unknown) {
     return { status: 'success' as const, message: 'Reset email sent!' }
   } catch (error) {
     console.error(`[PASSWORD_RESET_ACTION_ERROR] EMAIL:${email}`, error)
+    return {
+      status: 'error' as const,
+      message: 'An unexpected error occurred during password reset.',
+    }
+  }
+}
+
+export async function setNewPasswordAction(
+  values: z.infer<typeof NewPasswordSchema>,
+  token?: string,
+) {
+  if (!token) {
+    return { status: 'error' as const, message: 'Missing token!' }
+  }
+
+  try {
+    const validatedFields = NewPasswordSchema.safeParse(values)
+    if (!validatedFields.success) {
+      return {
+        status: 'error' as const,
+        message: 'Invalid input fields!',
+      }
+    }
+    const { password } = validatedFields.data
+    const existingToken = await getPasswoordResetTokenByToken(token)
+
+    if (!existingToken) {
+      return {
+        status: 'error' as const,
+        message: 'Token does not exist!',
+      }
+    }
+
+    const hasTokenExpired = new Date(existingToken.expires) < new Date()
+    if (hasTokenExpired) {
+      return {
+        status: 'error' as const,
+        message: 'Token has expired!',
+      }
+    }
+
+    const existingUser = await getUserByEmail(existingToken.email)
+    if (!existingUser) {
+      return {
+        status: 'error' as const,
+        message: 'Email does not exist!',
+      }
+    }
+
+    await updateUserPassword({ password, userId: existingUser.id })
+    await deletePasswordResetTokenById(existingToken.id)
+
+    return {
+      status: 'success' as const,
+      message: 'Password updated! Redirecting to login page...',
+    }
+  } catch (error) {
+    console.error('[RESET_PASSWORD_ACTION_ERROR]', error)
     return {
       status: 'error' as const,
       message: 'An unexpected error occurred during password reset.',
