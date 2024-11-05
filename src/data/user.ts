9581a5bcdf4a168b73ca8ddde5e2eaa8360bcf3d
Deveshwaar <deveshwaar99@gmail.com>
2024-11-05 18:26:13 +0530

    feat(password-reset): add function to update user password using user id

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,6 +3,7 @@
 import { db } from '@/db'
 import { users } from '@/db/schema'
 import { eq } from 'drizzle-orm'
+import bcryptjs from 'bcryptjs'
 
 export async function getUserByEmail(email: string) {
   try {
@@ -36,6 +37,23 @@ export async function verifyUserEmailById(id: string, email: string) {
     await db.update(users).set({ email, emailVerified: new Date() }).where(eq(users.id, id))
   } catch (error) {
     console.error('[VERIFY_USER_EMAIL_BY_ID_ERROR]', error)
+    throw new Error('Failed to verify user email.')
+  }
+}
+
+export async function updateUserPassword({
+  userId,
+  password,
+}: {
+  userId: string
+  password: string
+}) {
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 10)
+
+    await db.update(users).set({ password: hashedPassword }).where(eq(users.id, userId))
+  } catch (error) {
+    console.error('[UPDATE_USER_PASSWORD_ERROR]', error)
     throw new Error('Failed to verify user email.')
   }
 }
