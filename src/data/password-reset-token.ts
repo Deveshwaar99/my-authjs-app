e99c56b960d25f2b6d8f174fc5fe289543dd6390
Deveshwaar <deveshwaar99@gmail.com>
2024-11-05 16:52:15 +0530

    feat(password-reset): add functions for generating, retrieving, and deleting password reset tokens

---
+++ src/data/password-reset-token.ts
@@ -0,0 +1,59 @@
+import 'server-only'
+
+import { db } from '@/db'
+import { PasswordResetTokens } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+import { nanoid } from 'nanoid'
+
+export async function generatePasswordResetToken(email: string) {
+  try {
+    const token = nanoid()
+    const expires = new Date(Date.now() + 60 * 60 * 1000) // 1 hour from now
+
+    const passwordResetToken = await db
+      .insert(PasswordResetTokens)
+      .values({ token, email, expires })
+      .returning()
+      .then(res => res[0])
+
+    return passwordResetToken
+  } catch (error) {
+    console.error(`[GENERATE_VERIFICATION_TOKEN_ERROR] Email: ${email}`, error)
+    throw error
+  }
+}
+
+export async function deletePasswordResetTokenByEmail(email: string) {
+  try {
+    await db.delete(PasswordResetTokens).where(eq(PasswordResetTokens.email, email))
+  } catch (error) {
+    console.error(`[DELETE_PASSWORD_RESET_TOKEN_BY_EMAIL_ERROR] Email: ${email}`, error)
+    throw error
+  }
+}
+
+export async function getPasswoordResetTokenByToken(token: string) {
+  try {
+    return await db
+      .select()
+      .from(PasswordResetTokens)
+      .where(eq(PasswordResetTokens.token, token))
+      .then(res => res[0])
+  } catch (error) {
+    console.error('[GET_PASSWORD_RESET_TOKEN_BY_TOKEN_ERROR]', error)
+    return null
+  }
+}
+
+export async function getPasswoordResetTokenByEmail(email: string) {
+  try {
+    return await db
+      .select()
+      .from(PasswordResetTokens)
+      .where(eq(PasswordResetTokens.email, email))
+      .then(res => res[0])
+  } catch (error) {
+    console.error('[GET_PASSWORD_RESET_TOKEN_BY_TOKEN_ERROR]', error)
+    return null
+  }
+}
