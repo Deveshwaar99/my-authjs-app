34df0031012078d9dc62f0ec6df4f63ed64903ed
Deveshwaar <deveshwaar99@gmail.com>
2024-11-06 23:05:59 +0530

    feat(logout): add button to handle logout functionality

---
+++ src/components/auth/logout-buton.tsx
@@ -0,0 +1,13 @@
+import { logoutAction } from '@/actions/logout-action'
+import { Button } from '@/components/ui/button'
+import type { PropsWithChildren } from 'react'
+
+function LogoutButton({ children }: PropsWithChildren) {
+  return (
+    <Button onClick={() => logoutAction()} variant="destructive" className="cursor-pointer">
+      {children}
+    </Button>
+  )
+}
+
+export default LogoutButton
