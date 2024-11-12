205d78e2154b90ee20ebc388ae798cecf98fc803
Deveshwaar <deveshwaar99@gmail.com>
2024-11-12 13:43:25 +0530

    feat(auth): wrap ProtectedLayout with SessionProvider for session management
    - Added `SessionProvider` from `next-auth/react` to `ProtectedLayout` to provide session context
    - Ensures consistent session handling across protected routes and components

--- src/app/(root)/(protected)/layout.tsx
+++ src/app/(root)/(protected)/layout.tsx
@@ -1,12 +1,15 @@
 import type { PropsWithChildren } from 'react'
 import Navbar from './_components/navbar'
+import { SessionProvider } from 'next-auth/react'
 
 function ProtectedLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex size-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <Navbar />
-      {children}
-    </div>
+    <SessionProvider>
+      <div className="flex size-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+        <Navbar />
+        {children}
+      </div>
+    </SessionProvider>
   )
 }
 
