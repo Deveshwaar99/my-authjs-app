99356f3da43f7c275ad025c675a6cc20bf6e0051
Deveshwaar <deveshwaar99@gmail.com>
2024-11-12 17:34:31 +0530

    chore(toaster): add toaster component

--- src/app/(root)/(protected)/layout.tsx
+++ src/app/(root)/(protected)/layout.tsx
@@ -1,15 +1,19 @@
 import type { PropsWithChildren } from 'react'
 import Navbar from './_components/navbar'
 import { SessionProvider } from 'next-auth/react'
+import { Toaster } from '@/components/ui/toaster'
 
 function ProtectedLayout({ children }: PropsWithChildren) {
   return (
-    <SessionProvider>
-      <div className="flex size-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-        <Navbar />
-        {children}
-      </div>
-    </SessionProvider>
+    <>
+      <SessionProvider>
+        <div className="flex size-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+          <Navbar />
+          {children}
+        </div>
+      </SessionProvider>
+      <Toaster />
+    </>
   )
 }
 
