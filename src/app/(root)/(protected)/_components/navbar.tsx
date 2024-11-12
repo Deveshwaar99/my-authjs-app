b3ad6d36845fc78061c52f0480e362857483c565
Deveshwaar <deveshwaar99@gmail.com>
2024-11-12 13:44:05 +0530

    feat : add User button in navbar

--- src/app/(root)/(protected)/_components/navbar.tsx
+++ src/app/(root)/(protected)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import UserButton from '@/components/auth/user-button'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -23,6 +24,7 @@ function Navbar() {
           <Link href="/settings">Settings</Link>
         </Button>
       </div>
+      <UserButton />
     </nav>
   )
 }
