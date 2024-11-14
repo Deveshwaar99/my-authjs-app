e2e1a549a7c104484077cb554167d727ab4b9982
Deveshwaar <deveshwaar99@gmail.com>
2024-11-14 14:07:23 +0530

    feat(logout): redirect to home after logout

--- src/actions/logout-action.ts
+++ src/actions/logout-action.ts
@@ -1,10 +1,12 @@
 'use server'
 
 import { signOut } from '@/auth'
+import { redirect } from 'next/navigation'
 
 export async function logoutAction() {
   try {
     await signOut()
+    redirect('/')
   } catch (error) {
     console.error('[LOGOUT_ACTION_ERROR]', error)
     return { status: 'error', message: 'Somthing went wrong!' }
