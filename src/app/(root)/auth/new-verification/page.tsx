811e5fcb3139f6fd5eaa276a690d4884d2a0713b
Deveshwaar <deveshwaar99@gmail.com>
2024-11-16 00:28:35 +0530

    chore : fix imports

--- src/app/(root)/auth/new-verification/page.tsx
+++ src/app/(root)/auth/new-verification/page.tsx
@@ -1,4 +1,4 @@
-import NewVerificationForm from '@/components/auth/new-verification-form'
+import { NewVerificationForm } from '@/components/auth/new-verification-form'
 
 function NewVerificationPage() {
   return <NewVerificationForm />
